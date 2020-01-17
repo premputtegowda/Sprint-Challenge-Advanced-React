e52a4fe59d4b8a0b18a764e8aae612e969aeb346
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-01-17 08:33:35 -0800

    displaying the dta from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+class App extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+      players: []
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount(){
+    axios.get("http://localhost:5000/api/players")
+    .then(res => this.setState({ players: res.data}))
+    .catch(err => { console.log(err)})
+
+  }
+  render() {
+    return (
+      <div className="App">
+        
+        {this.state.players.map((player, index) => {
+          return (
+            <div key={index}>
+              {player.name}, Country: {player.country}, Searches: {player.searches}
+              <hr />
+              
+            </div>
+          )
+        })
+        }
+
+      </div>
+    );
+  }
 }
-
 export default App;
